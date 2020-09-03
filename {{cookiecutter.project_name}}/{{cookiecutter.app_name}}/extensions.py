8784a2619bc306c9d78c2618e04799653243966f
zekaio <zekailing@gmail.com>
2020-09-03 16:02:46 +0800

    .

--- {{cookiecutter.project_name}}/{{cookiecutter.app_name}}/extensions.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.app_name}}/extensions.py
@@ -5,24 +5,32 @@
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy, Model
-from sqlalchemy.sql.schema import Column
-from sqlalchemy.sql.sqltypes import DateTime
-from sqlalchemy import func
+from sqlalchemy import Column, DateTime, func
+from sqlalchemy.orm import Query, Session
 
 
 cors = CORS(supports_credentials=True)
 migrate = Migrate()
 
-class BaseModel(Model):
-    __table_args__ = {
-        'mysql_charset': 'utf8mb4'
-    }
+
+class _SQLAlchemy(SQLAlchemy):
+    session: Session
+
+
+db = _SQLAlchemy()
+
+
+class _Model(db.Model):
+    __abstract__ = True
+
+    query: Query
 
     created_at = Column(DateTime, server_default=func.now(), comment='创建时间')
-    updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now(), comment='修改时间')
+    updated_at = Column(DateTime, server_default=func.now(),
+                        onupdate=func.now(), comment='修改时间')
 
     def to_dict(self):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
 
-db = SQLAlchemy(model_class=BaseModel)
+db.Model = _Model
