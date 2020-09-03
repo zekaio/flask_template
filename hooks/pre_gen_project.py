a378b239f116a3ef27f6d24ffca2a306b1ed0398
zekaio <zekailing@gmail.com>
2020-09-03 22:16:57 +0800

    .

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -6,7 +6,7 @@ import sys
 
 MODULE_REGEX = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
 
-module_name = '{{ cookiecutter.project_name}}'
+module_name = '{{ cookiecutter.app_name}}'
 
 if not re.match(MODULE_REGEX, module_name):
     print('ERROR: The project name (%s) is not a valid Python module name. Please do not use a - and use _ instead' % module_name)
